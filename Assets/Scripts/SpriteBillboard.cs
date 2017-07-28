dd4a7e2ccc6fab1a3225c05c52e8de3d077b61ab
SAE\zcroslow <1005440@student.sae.edu.au>
2017-07-28 16:33:49 +1000

    Sprites now billboard with player camera movement. - Zac

---
+++ Assets/Scripts/SpriteBillboard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteBillboard : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    void Update()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up); // it makes the object point in the same direction as the camera's forward axis
+    }
+}
