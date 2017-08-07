472a7eba8d4ab271f7dd49df8b87c67c9c4f018f
DESKTOP-G3UB1RN\Ruby Simpson <rubysimpsonwork@gmail.com>
2017-08-07 16:38:24 +1000

    Working on exit door

---
+++ Assets/Scripts/ExitManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitManager : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        //check if convo is happening
+        //if not execute fungus
+    }
+}
