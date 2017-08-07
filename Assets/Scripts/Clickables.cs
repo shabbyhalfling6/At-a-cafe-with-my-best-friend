23976e159e9bbdc627db51e7b4fb55517db0fff1
DESKTOP-G3UB1RN\Ruby Simpson <rubysimpsonwork@gmail.com>
2017-08-07 18:14:08 +1000

    Traded the exitdoor script for clickables script.  Takes a flowchart and block name and then makes sure your interactable object options don't interrupt your main conversation

---
+++ Assets/Scripts/Clickables.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Clickables : MonoBehaviour {
+
+    private GameObject manager; //the object the conversationmanager script is attached to
+    public Flowchart appropriateFlowchart; //
+    public string blockName;
+
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("Manager");
+    }
+
+    void OnMouseDown()
+    {
+        if (manager.GetComponent<ConversationManager>().havingConvo == false)
+        {
+            appropriateFlowchart.ExecuteBlock(blockName);
+        }
+    }
+}
