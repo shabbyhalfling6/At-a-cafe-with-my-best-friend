12cf5e868d873aa21b3973c06437f864055b8e00
DESKTOP-G3UB1RN\Ruby Simpson <rubysimpsonwork@gmail.com>
2017-08-08 13:49:01 +1000

    Buttons work with buttons (press keypad 1 for 1st option, keypad 2 for 2nd option)

---
+++ Assets/Scripts/BasicFunctions/FungusButtonPress.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+using UnityEngine.EventSystems;
+
+public class FungusButtonPress : MonoBehaviour {
+    
+    public KeyCode keycode;
+    	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(keycode))
+        {
+            ExecuteEvents.Execute<IPointerClickHandler>(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+        }
+	}
+}
