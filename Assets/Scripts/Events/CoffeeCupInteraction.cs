e616c021cb4f5b81eb45d2bcb0fc02452c569db9
shabbyhalfling6 <bradycjohnston@hotmail.com>
2017-08-08 20:10:54 +1000

    Updated Jounral and sipping drink
    - Sipping drink sprites
    - Updated journal to be multiple pages, has an exit button instead of the escape button, and other stuff

--- Assets/Scripts/Events/CoffeeCupInteraction.cs
+++ Assets/Scripts/Events/CoffeeCupInteraction.cs
@@ -5,27 +5,30 @@ using Fungus;
 
 public class CoffeeCupInteraction : MonoBehaviour {
 
-    public int clicksBeforeEmpty = 5;
+    public int clicksBeforeEmpty = 4;
+    public int clicksBeforeEmptyIn = 4;
 
     public Flowchart eventFlowchart;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Sprite[] coffeeCupSprites;
+    public Sprite[] teaCupSprites;
 
     void OnMouseDown()
     {
+        //take a sip from the drink
         clicksBeforeEmpty--;
 
-        if(clicksBeforeEmpty >= 0)
+        //if the cup is empty
+        if(clicksBeforeEmpty == 0)
         {
+            //execute the player served event in fungus so the barista will serve the player
             eventFlowchart.ExecuteBlock("PlayerServed");
+            this.GetComponent<SpriteRenderer>().sprite = coffeeCupSprites[0];
+        }
+        else
+        {
+            //change the sprite to the current level of sip (only coffee atm)
+            this.GetComponent<SpriteRenderer>().sprite = coffeeCupSprites[clicksBeforeEmpty];
         }
     }
 
--- Assets/Scripts/Events/JournalManager.cs
+++ Assets/Scripts/Events/JournalManager.cs
@@ -8,45 +8,19 @@ public class JournalManager : MonoBehaviour {
     public GameObject manager; //this is where convomanger is held.  when click on journal it'll check against the havingConvo bool and if the friends are having a convo it won't open the journal
     private GameObject cameraMain; //this is the camera that the followmouse script is held on.  I'll need to access it
 
-    public GameObject journalTextObject;
-    public InputField journalInputField;
+    public GameObject[] journalTextObject;
+    //public InputField journalInputField;
 
-    public int journalOpened;
+    //analytic for times the journal was opened
+    public int journalOpened = 0;
+
+    public int currentJournalPage = 0;
 
 	// Use this for initialization
 	void Start ()
     {
         cameraMain = GameObject.FindGameObjectWithTag("MainCamera");
 	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        /*
-        //raycast the mouse point to the game world to test if you are clicking something
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                if()
-                //activate journal object in scene
-                journalTextObject.SetActive(true);
-                Cursor.visible = true;
-            }
-        }*/
-
-        //escape the writing mode when pressing escape
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            journalTextObject.SetActive(false);
-            manager.GetComponent<ConversationManager>().havingConvo = false;
-            cameraMain.GetComponent<MouseLook>().FreeView();
-        }
-
-    }
 
     void OnMouseDown()
     {
@@ -54,10 +28,29 @@ public class JournalManager : MonoBehaviour {
         {
             manager.GetComponent<ConversationManager>().havingConvo = true;
             cameraMain.GetComponent<MouseLook>().LookTarget(gameObject, 5f);
-            journalInputField.ActivateInputField();
-            journalTextObject.SetActive(true);
+            //journalInputField.ActivateInputField();
+            journalTextObject[currentJournalPage].SetActive(true);
             journalOpened++;
         }
+    }
+
+    public void OnButtonSwitchPage(int pageUpOrDown)
+    {
+        if (currentJournalPage < journalTextObject.Length)
+        {
+            int previousJournalPage = currentJournalPage;
+            currentJournalPage += pageUpOrDown;
+            journalTextObject[currentJournalPage].SetActive(true);
+            journalTextObject[previousJournalPage].SetActive(false);
+        }
+    }
+
+    public void OnButtonExitJournal()
+    {
+        //escape the writing mode when button is pressed
+        journalTextObject[currentJournalPage].SetActive(false);
+        manager.GetComponent<ConversationManager>().havingConvo = false;
+        cameraMain.GetComponent<MouseLook>().FreeView();
     }
 
 }
