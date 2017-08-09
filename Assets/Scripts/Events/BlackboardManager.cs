5798947919ddc62ad13027d208fadfeb930a047a
SAE\rsimpson2 <rubysimpsonwork@gmail.com>
2017-08-09 11:53:51 +1000

    blackboard + outside changes

--- Assets/Scripts/Events/BlackboardManager.cs
+++ Assets/Scripts/Events/BlackboardManager.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 
 public class BlackboardManager : MonoBehaviour {
 
-    private string[] puns = { "Our thickshakes are dairy good <3", "Maybe she's born with it, maybe it's caffeine", "It's a brew-tiful day!", "Iced coffee?? Cool beans!" };
+    private string[] puns = { "Our thickshakes are dairy good <3", "Maybe she's born with it, maybe it's caffeine", "It's a brew-tiful day!", "Iced coffee?? Cool beans!", "Livin' la vida mocha", "I like big cups and I cannot lie", "Have a cup of positivi-tea!" };
     private string currentPun;
     public Text textbox;
 
 	// Use this for initialization
 	void Start () {
-        currentPun = puns[0];
-	}
+        int i = Random.Range(0, puns.Length);
+        textbox.text = puns[i];
+        currentPun = puns[i];
+    }
 
     public void NewBlackboard()
     {
