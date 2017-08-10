9d05c05b3dd84df35824b766139c337f1610ad73
SAE\zcroslow <1005440@student.sae.edu.au>
2017-08-10 15:07:23 +1000

    Created a _MainMenuScene, Added a Main Menu image, Hooked up buttons to load Gamescene from MainMenu.  There is an issue with loading the Gamescene's directional light. - Zac

---
+++ Assets/Scripts/MainMenu Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame(string GameScene)
+    {
+        SceneManager.LoadScene(GameScene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
