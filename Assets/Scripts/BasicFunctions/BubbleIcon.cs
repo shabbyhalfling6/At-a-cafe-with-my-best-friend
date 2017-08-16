5cb09128da6e0bee109b1b9c159685779d9d5af2
DESKTOP-G3UB1RN\Ruby Simpson <rubysimpsonwork@gmail.com>
2017-08-16 23:01:08 +1000

    Started throwing together an icon for when the ladies are having a conversation. Needs to be put on a canvas or something though, right now it's just attached to the main camera

---
+++ Assets/Scripts/BasicFunctions/BubbleIcon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleIcon : MonoBehaviour {
+
+    public ConversationManager manager;
+
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<ConversationManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(manager.havingConvo == true)
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+	}
+}
