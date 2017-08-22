f00249953834541c1843a7a76367dac91ba29565
SAE\zcroslow <1005440@student.sae.edu.au>
2017-08-23 09:36:53 +1000

    Fixed sams name - Zac

---
+++ Assets/Scripts/MainMenu Scripts/SliderControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderControl : MonoBehaviour
+{
+    public Slider Volume;
+    public AudioSource myMusic;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
